e1ca829a345b2b94996fbd0ef7a4e4c1e3974db2
zwyang <celonx@gmail.com>
2017-03-10 12:40:59 +0800

    add yum repo for nginx

--- init/installNginx.sh
+++ init/installNginx.sh
@@ -8,23 +8,46 @@ source $DIR/util/util.sh
 
 setupBasicEnv
 os=$( osinfo )
+USER=$( whoami )
+if [[ $USER != 'root' ]]; then
+	echo Current user must be root.
+	exit -1
+fi
+
+if [[ $os == "CentOS Linux release 7"* ]]; then
+	if [[ ! -f /etc/yum.repos.d/nginx.repo ]]; then
+		echo "[nginx]" > /etc/yum.repos.d/nginx.repo
+		echo "name=nginx repo" >> /etc/yum.repos.d/nginx.repo
+		echo "baseurl=http://nginx.org/packages/centos/7/\$basearch/" >> /etc/yum.repos.d/nginx.repo
+		echo "gpgcheck=0" >> /etc/yum.repos.d/nginx.repo
+		echo "enabled=1" >> /etc/yum.repos.d/nginx.repo
+	fi
+elif [[ $os == "CentOS Linux release 6"* ]]; then
+	if [[ ! -f /etc/yum.repos.d/nginx.repo ]]; then
+		echo "[nginx]" > /etc/yum.repos.d/nginx.repo
+		echo "name=nginx repo" >> /etc/yum.repos.d/nginx.repo
+		echo "baseurl=http://nginx.org/packages/centos/6/\$basearch/" >> /etc/yum.repos.d/nginx.repo
+		echo "gpgcheck=0" >> /etc/yum.repos.d/nginx.repo
+		echo "enabled=1" >> /etc/yum.repos.d/nginx.repo
+	fi
+fi
 
 for app in nginx
 do
 	checkBinPath $app && continue
 	if [[ $os == CentOS* ]]; then
-		isSudoAllowed || abort "Must be sudo allowed"
-		statusExec sudo yum -y install $app
+		isSudoAllowed || abort "Must be allowed"
+		statusExec yum -y install $app
 	elif [[ $os == Ubuntu* ]]; then
-		isSudoAllowed || abort "Must be sudo allowed"
-		statusExec sudo apt-get -y install $app
+		isSudoAllowed || abort "Must be allowed"
+		statusExec apt-get -y install $app
 	elif [[ $os == "Darwin" ]]; then
 		statusExec brew install $app
 	fi
 done
 
 if [[ $os == CentOS* ]]; then
-	statusExec sudo yum -y install nginx-all-modules
+	statusExec yum -y install nginx-module-*
 elif [[ $os == Ubuntu* ]]; then
 	:
 elif [[ $os == "Darwin" ]]; then
