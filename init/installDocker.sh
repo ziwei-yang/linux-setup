0f08b783b9b27ae648abd5a52c18525313c6e1a5
zwyang <celonx@gmail.com>
2017-03-20 15:06:54 +0800

    add dock test for installDock

--- init/installDocker.sh
+++ init/installDocker.sh
@@ -7,6 +7,7 @@ DIR=$DIR/../
 source $DIR/util/util.sh
 
 setupBasicEnv
+USER_INSTALL="$HOME/install"
 os=$( osinfo )
 if [[ $os = "CentOS Linux release 7."* ]]; then
 	if [[ $os < "CentOS Linux release 7.3" ]]; then
@@ -27,6 +28,10 @@ if [[ $os = "CentOS Linux release 7."* ]]; then
 	statusExec sudo systemctl start docker
 	statusExec docker run hello-world || \
 		echoBlue "Log out and log back in so that your group membership is re-evaluated."
+	statusExec wget --quiet -O /tmp/docker-machine https://github.com/docker/machine/releases/download/v0.10.0/docker-machine-`uname -s`-`uname -m` && \
+		chmod +x /tmp/docker-machine && \
+		statusExec mv /tmp/docker-machine $USER_INSTALL/bin/
+	statusExec $USER_INSTALL/bin/docker-machine --version
 else
 	abort "$os is not supported"
 fi
