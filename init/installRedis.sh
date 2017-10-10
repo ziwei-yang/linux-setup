c7e2dc5f7c299b89bb322928a9875bd5a72d85cc
zwyang <yangziwei@me.com>
2017-10-10 16:01:07 +0800

    add redis-stat into redis suite

--- init/installRedis.sh
+++ init/installRedis.sh
@@ -28,9 +28,7 @@ echoGreen "-------- Installing redis-commander --------"
 checkExactBinPath "redis-commander" $USER_INSTALL/bin/redis-commander
 ret=$?
 if [ $ret == "0" ]; then
-	echoBlue "Reinstall redis-commander."
-	npm uninstall -g redis-commander
-	npm install -g https://github.com/MegaGM/redis-commander.git
+	echoBlue "Skip redis-commander."
 else
 	npm install -g redis-commander
 	npm install -g https://github.com/MegaGM/redis-commander.git
