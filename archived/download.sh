9561503bc613c6d7b3355cad8f7d24c6e4796059
zwyang <yangziwei@me.com>
2018-06-01 16:15:15 +0800

    use NORUBY in archive/download.sh

--- archived/download.sh
+++ archived/download.sh
@@ -2,7 +2,7 @@
 PWD=$(pwd)
 SOURCE="${BASH_SOURCE[0]}"
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-source $DIR/../common/bootstrap.sh
+source $DIR/../common/bootstrap.sh NORUBY
 
 uname=$( uname )
 source $DIR/../util/util.sh
