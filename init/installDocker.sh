a6a0ae2082b05b7b3943ac798cc8baa6d8df20bb
zwyang <yangziwei@me.com>
2018-06-13 16:32:44 +0800

    docker install/uninstall updated

--- init/installDocker.sh
+++ init/installDocker.sh
@@ -2,11 +2,10 @@
 PWD=$(pwd)
 SOURCE="${BASH_SOURCE[0]}"
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-source $DIR/../common/bootstrap.sh
+source $DIR/../common/bootstrap.sh NORUBY
 
 os=$( osinfo )
 if [[ $os = "CentOS Linux release 7."* ]]; then
-	require_version='17.09'
 	if [[ $os < "CentOS Linux release 7.3" ]]; then
 		abort "$os is not supported"
 	fi
--- init/uninstallDocker.sh
+++ init/uninstallDocker.sh
@@ -2,17 +2,28 @@
 PWD=$(pwd)
 SOURCE="${BASH_SOURCE[0]}"
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-source $DIR/../common/bootstrap.sh
+source $DIR/../common/bootstrap.sh NORUBY
 
 USER_INSTALL="$HOME/install"
 os=$( osinfo )
 if [[ $os = "CentOS Linux release 7."* ]]; then
-	# version: 17.09
 	if [[ $os < "CentOS Linux release 7.3" ]]; then
 		abort "$os is not supported"
 	fi
 	can_sudo || abort "User has no privilege."
-	status_exec sudo yum remove docker docker-common docker-selinux docker-engine
+	status_exec sudo yum -y remove docker \
+		docker-client \
+		docker-client-latest \
+		docker-common \
+		docker-latest \
+		docker-latest-logrotate \
+		docker-logrotate \
+		docker-selinux \
+		docker-engine-selinux \
+		docker-engine
+	if [ -d /var/lib/docker/ ]; then
+		log "/var/lib/docker/ still exists, purge it if needed"
+	fi
 else
 	abort "$os is not supported"
 fi
