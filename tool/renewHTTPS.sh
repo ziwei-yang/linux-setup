89912aa15014a5abeeeef48396568ac6db4fea54
zwyang <yangziwei@me.com>
2019-02-18 11:47:43 +0800

    renew letsencrypt cert script added

--- tool/renewHTTPS.sh
+++ tool/renewHTTPS.sh
@@ -7,4 +7,4 @@ source $DIR/../common/bootstrap.sh NORUBY NOPYTHON
 cd /tmp
 status_exec wget https://dl.eff.org/certbot-auto
 status_exec chmod u+x /tmp/certbot-auto
-sudo /tmp/certbot-auto renew
+sudo /tmp/certbot-auto renew && status_exec sudo service nginx reload
