eb9b2b51735f684f50f178ccf6165b82648a4a70
zwyang <yangziwei@me.com>
2019-02-18 14:08:04 +0800

    apply https cert script added

---
+++ tool/applyHTTPS.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+PWD=$(pwd)
+SOURCE="${BASH_SOURCE[0]}"
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+source $DIR/../common/bootstrap.sh NORUBY NOPYTHON
+
+cd /tmp
+status_exec wget https://dl.eff.org/certbot-auto
+status_exec chmod u+x /tmp/certbot-auto
+sudo /var/nginx/certbot-auto certonly --webroot \
+        --webroot-path=/var/nginx/www \
+	$@ # -d domain1 -d domain2
