74215cf308829575cfd825e41484428e93307729
hh <357866872@qq.com>
2020-10-28 10:48:53 +0800

    Update Conda bash

--- init/initConda.sh
+++ init/initConda.sh
@@ -10,11 +10,14 @@ os=$( osinfo )
 # use; install
 if [[ $os == CentOS* ]]; then
   if [[ $1 == "install" ]]; then
-    status_exec ~/miniconda/bin/conda create --name $2 $3
+    status_exec ~/miniconda/bin/conda create --name $2 python=$3
   elif [[ $1 == "use" ]]; then
     status_exec source ~/miniconda/bin/activate $2
+  elif [[ $1 == "envs" ]]]; then
+    ~/miniconda/bin/conda info --envs
   elif [[ $1 == "help" ]]; then
-    echo "install env_name python_ver"
-    echo "use env_name"
+    echo "install env_name python_ver. Example: install py3.8 3.8"
+    echo "use env_name. Example: use py3.8"
+    ehco "envs. Example: envs"
   fi
 fi
