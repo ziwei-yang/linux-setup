bbc3c77d4cce5d705c4555c21eb9e982dcb02370
zwyang <yangziwei@me.com>
2021-05-11 22:28:25 +0800

    redis additional tools removed

--- init/installRedis.sh
+++ init/installRedis.sh
@@ -22,24 +22,3 @@ else
 	make PREFIX=$USER_INSTALL install -j $MAKE_CORE_NUM > /dev/null || make PREFIX=$USER_INSTALL install > /dev/null
 fi
 assert_path "redis-server"
-
-log_green "-------- Installing redis-commander --------"
-check_path "redis-commander" $USER_INSTALL/bin/redis-commander
-ret=$?
-if [ $ret == "0" ]; then
-	log_blue "Skip redis-commander."
-else
-	npm install -g redis-commander
-	npm install -g https://github.com/MegaGM/redis-commander.git
-fi
-assert_path "redis-commander"
-
-log_green "-------- Installing redis-stat --------"
-find_path "redis-stat"
-ret=$?
-if [ $ret == "0" ]; then
-	log_blue "Skip redis-stat."
-else
-	gem install redis-stat
-fi
-assert_path "redis-stat"
