6b2d03477e6955171e3b588bad5691ed61029fa1
ziwei yang <zwyang@ziweis-Mac-mini.local>
2021-06-01 21:45:00 +0800

    tmux_dev removed, use tmux.sh instead

--- conf/home/tmux_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-tmux has-session -t dev
-if [ $? != 0 ]; then
-	tmux new-session -s dev -n bash -d
-	#tmux new-session -s dev -n sql -d
-	#tmux send-keys -t dev:sql 'mysql -uroot -p' C-m
-fi
-
-tmux attach -t dev
