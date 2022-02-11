17bfa41ba6855a9eaab7d8ff3da23cf1759e1f78
ziwei yang <yangziwei@me.com>
2022-02-11 17:07:13 +0800

    install redis on macos with brew

--- init/installRedis.sh
+++ init/installRedis.sh
@@ -6,19 +6,24 @@ DIR=$DIR/../
 source $DIR/common/bootstrap.sh NORUBY
 
 log_green "-------- Installing Redis -------"
-check_path "redis-server" $USER_INSTALL/bin/redis-server
+find_path "redis-server"
 ret=$?
 if [ $ret == "0" ]; then
 	log_blue "Skip redis."
 else
-	filename=$(basename $( ls $DIR/archived/redis-* ))
-	[ -d $USER_ARCHIVED ] || mkdir -p $USER_ARCHIVED
-	cp -v $DIR/archived/$filename $USER_ARCHIVED/
-	cd $USER_ARCHIVED
-	tar -xf $filename
-	dirname=${filename%.tar.gz}
-	cd $USER_ARCHIVED/$dirname
-	log_blue "make PREFIX=$USER_INSTALL install -j $MAKE_CORE_NUM > /dev/null"
-	make PREFIX=$USER_INSTALL install -j $MAKE_CORE_NUM > /dev/null || make PREFIX=$USER_INSTALL install > /dev/null
+	is_mac && (
+		brew install redis
+	)
+	is_linux && (
+		filename=$(basename $( ls $DIR/archived/redis-* ))
+		[ -d $USER_ARCHIVED ] || mkdir -p $USER_ARCHIVED
+		cp -v $DIR/archived/$filename $USER_ARCHIVED/
+		cd $USER_ARCHIVED
+		tar -xf $filename
+		dirname=${filename%.tar.gz}
+		cd $USER_ARCHIVED/$dirname
+		log_blue "make PREFIX=$USER_INSTALL install -j $MAKE_CORE_NUM > /dev/null"
+		make PREFIX=$USER_INSTALL install -j $MAKE_CORE_NUM > /dev/null || make PREFIX=$USER_INSTALL install > /dev/null
+	)
 fi
 assert_path "redis-server"
